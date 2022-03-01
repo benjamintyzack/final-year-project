c16bb24d98d0a2172e80039a234a47ebd04e53b5
benjamintyzack <b7008109@my.shu.ac.uk>
2022-03-01 14:42:53 +0000

    Buttons added to settings page to sign out and delete account with functionality, slight xml change to the workout fragment

--- app/src/main/java/com/finalyearproject/app/DeleteAccountFragment.java
+++ app/src/main/java/com/finalyearproject/app/DeleteAccountFragment.java
@@ -11,15 +11,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
+ * Use the {@link DeleteAccountFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SettingsFragment extends Fragment implements OnClickListener {
+public class DeleteAccountFragment extends Fragment implements OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,9 +34,10 @@ public class SettingsFragment extends Fragment implements OnClickListener {
     private String mParam1;
     private String mParam2;
 
-    private Button signOutButton, changePasswordButton, deleteAccountButton;
+    private Button confirmDeleteButton;
+    private EditText reEnterPassword;
 
-    public SettingsFragment() {
+    public DeleteAccountFragment() {
         // Required empty public constructor
     }
 
@@ -42,11 +47,11 @@ public class SettingsFragment extends Fragment implements OnClickListener {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
+     * @return A new instance of fragment DeleteAccountFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
+    public static DeleteAccountFragment newInstance(String param1, String param2) {
+        DeleteAccountFragment fragment = new DeleteAccountFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -67,37 +72,38 @@ public class SettingsFragment extends Fragment implements OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_delete_account, container, false);
 
-        signOutButton = view.findViewById(R.id.signOut);
-        changePasswordButton = view.findViewById(R.id.changePassword);
-        deleteAccountButton = view.findViewById(R.id.deleteAccount);
+        confirmDeleteButton = view.findViewById(R.id.confirmDelete);
+        reEnterPassword = view.findViewById(R.id.confirmPassword);
 
-        signOutButton.setOnClickListener(this);
-        deleteAccountButton.setOnClickListener(this);
+        confirmDeleteButton.setOnClickListener(this);
         // Inflate the layout for this fragment
         return view;
     }
 
-
-
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.signOut:
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(getActivity(), MainActivity.class));
-                break;
-            case R.id.changePassword:
-                changePassword();
-                break;
-            case R.id.deleteAccount:
-                getParentFragmentManager().beginTransaction().replace(R.id.fragment_container, new DeleteAccountFragment()).commit();
+            case R.id.confirmDelete:
+                deleteAccount();
                 break;
         }
     }
 
-    public void changePassword() {
+    public void deleteAccount() {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
+        AuthCredential credential = EmailAuthProvider
+                .getCredential(user.getEmail(), reEnterPassword.getText().toString());
+
+        // Prompt the user to re-provide their sign-in credentials
+        user.reauthenticate(credential)
+                .addOnCompleteListener(task -> user.delete()
+                        .addOnCompleteListener(task1 -> {
+                            if (task1.isSuccessful()) {
+                                startActivity(new Intent(getActivity(), MainActivity.class));
+                            }
+                        }));
     }
 }
